f0b80d613becc7842ecfe37def613a8f1d2e7674
hayder <hayderbd4290@gmail.com>
2026-05-10 21:24:48 +0600

    get single data api create

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { getProduct } from "../services/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -8,10 +9,22 @@ export const productController = (
   const url = req.url;
   const method = req.method;
 
-  const product = getProduct()
+  const urlPart = url?.split("/");
 
-  if (url?.startsWith("/products") && method === "GET") {
+  const id = urlPart && urlPart[1] === "products" ? Number(urlPart[2]) : null;
+
+  const product = getProduct();
+
+  if (url === "/products" && method === "GET") {
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "this is product route", data: product }));
+    res.end(
+      JSON.stringify({ message: "this is product route", data: product }),
+    );
+  } 
+  else if (method === "GET" && id !== null) {
+    const product = getProduct();
+    const singleProduct = product.find((item:IProduct) => item.id === id);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "product route", data: singleProduct }));
   }
 };
